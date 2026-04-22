017a7b317857bfd5c337cddf9816e639c41e0ddb
Veronika Hulakova <hulakova.veronika@seznam.cz>
2026-04-22 09:28:34 +0200

    Add bubble sort function

--- sorting.py
+++ sorting.py
@@ -1,9 +1,8 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
-
-
 
 def selection_sort(values):
     for j in range(len(values)):
@@ -17,8 +16,30 @@ def selection_sort(values):
         print(values)
     return values
 
+def bubble_sort(values):
+    plt.ion()
+    plt.show()
+    for j in range(len(values)-1):
+        for i in range(len(values)-j-1):
+            index_highlight1 = i
+            index_highlight2 = i + 1
+            colors = ["steelblue"] * len(values)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(values)), values, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+            if values[i] > values[i+1]:
+                values[i], values[i+1] = values[i+1], values[i]
+    plt.ioff()
+    plt.show()
+    return values
+
+
 if __name__ == "__main__":
     values = random_numbers(10)  # 10 čísel v rozsahu 0–100
-    print(selection_sort(values))
+    #print(selection_sort(values))
+    print(bubble_sort(values))
     # print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
     # small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
